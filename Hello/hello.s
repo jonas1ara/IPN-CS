a0f9d065b964ad977f1e014f51a56615b8e2d3ab
Jonas-Lara <adrianlr2515@outlook.com>
2021-04-01 19:46:23 -0600

    Hello

---
+++ Hello/hello.s
@@ -0,0 +1,27 @@
+	.file	"hello.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Hello world"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$.LC0, %edi
+	call	puts
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 11.0.1 20210324 (Red Hat 11.0.1-0)"
+	.section	.note.GNU-stack,"",@progbits
