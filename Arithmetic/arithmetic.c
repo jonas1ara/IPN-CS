22f60e0104f1e78737e3aa4d3c24e1ab7d739862
Jonas-Lara <adrianlr2515@outlook.com>
2021-04-02 14:25:13 -0600

    Arithmetics

--- Arithmetic/arithmetic.c
+++ Arithmetic/arithmetic.c
@@ -3,15 +3,19 @@
 int main(void) {
 	
 	int num1, num2, resultado;
+	int a=2;
+	int b=3;
 	
-	printf("Ingrese un número:\n");
+	printf("Ingrese un número:\n");	
 	scanf("%d",&num1);
 
 	printf("Ingreso el segundo número:\n");
 	scanf("%d",&num2);
 
-	resultado = num1 / num2; //Cambiar operador por cualquiera de los operadores aritméticos; suma, resta, división y multiplicación. Vuelve a compilar.
-
+	/*resultado = num1 / num2;*/ //Cambiar operador por cualquiera de los operadores aritméticos; suma, resta, división y multiplicación. Vuelve a compilar.
+	
+	resultado = a*(num1-num2) / b*(num1+num2);
+	
 	printf("El resultado de la operación es: %d\n", resultado);
 
 	return 0;
