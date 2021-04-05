c432f029fda8be112d7279dbf347229f79cdfae6
Jonas-Lara <adrianlr2515@outlook.com>
2021-04-05 14:23:06 -0500

    directives

--- Directives/directives.s
+++ Directives/directives.s
@@ -1,10 +1,9 @@
 	.file	"directives.c"
 	.text
 	.section	.rodata
+	.align 8
 .LC0:
-	.string	"Hi"
-.LC1:
-	.string	""
+	.string	"The value of the variable is:  %i\n"
 	.text
 	.globl	main
 	.type	main, @function
@@ -16,14 +15,17 @@ main:
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$3, -4(%rbp)
+	movl	-4(%rbp), %eax
+	imull	%eax, %eax
+	imull	-4(%rbp), %eax
+	movl	%eax, %esi
 	movl	$.LC0, %edi
 	movl	$0, %eax
 	call	printf
-	movl	$.LC1, %edi
 	movl	$0, %eax
-	call	__isoc99_scanf
-	movl	$0, %eax
-	popq	%rbp
+	leave
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
