7e9dffab7766e33860e3ed0efbf08cd62f3f1eb6
Jonas-Lara <adrianlr2515@outlook.com>
2021-04-04 20:09:58 -0500

    yaa

---
+++ Directives/directives.s
@@ -0,0 +1,33 @@
+	.file	"directives.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Hi"
+.LC1:
+	.string	""
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$.LC0, %edi
+	movl	$0, %eax
+	call	printf
+	movl	$.LC1, %edi
+	movl	$0, %eax
+	call	__isoc99_scanf
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 11.0.1 20210324 (Red Hat 11.0.1-0)"
+	.section	.note.GNU-stack,"",@progbits
