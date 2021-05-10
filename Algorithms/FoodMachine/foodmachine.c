314cca56f4c75198c1ea324ce07c6af0b49a62f5
Jonas-Lara <adrianlr2515@outlook.com>
2021-05-09 20:12:34 -0500

    ssg test

---
+++ Algorithms/FoodMachine/foodmachine.c
@@ -0,0 +1,20 @@
+/*
+
+	Program Name: Food Machine
+	Date: 09/05/21
+	Author: Adrián Jonás Lara Carvente
+	Description: 
+
+*/
+
+
+
+#include <stdio.h>
+
+int maint(){
+	
+	printf("");
+	
+	return 0;
+
+}
