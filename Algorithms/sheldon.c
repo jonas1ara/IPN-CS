9dea744ab0888d919ffd49bf33a9205610c52e44
Jonas Lara <61485427+Jonas-Lara@users.noreply.github.com>
2021-10-03 16:53:12 -0500

    semi

--- Algorithms/sheldon.c
+++ Algorithms/sheldon.c
@@ -8,15 +8,17 @@
 #include <stdio.h>
 
 int main() {
-    int primos_por_mostrar, n, d, numero, resto;
+    int primos_por_mostrar, n = 2, d, numero, numero_limite , resto;
     int es_primo; //Se usara como booleano
+    int primo_final;
 
     printf("Este programa muestra la cantidad de números primos pedida y si su número espejo es primo\n"); 
-    printf("¿Cuantos primos desea mostrar? "); 
+    printf("Digite el número limite de números primos: ");
     scanf("%d", &primos_por_mostrar); 
 
-    n = 2; //contador pa empezar con los primos, porque pues el primer primo es 2 :D
-    while (primos_por_mostrar > 0) { //8 > 0
+   // n = 2; //contador pa empezar con los primos, porque pues el primer primo es 2 :D
+    
+    while (primos_por_mostrar > numero_limite ) { //8 > 0
 
         /* determinar si n es primo */
         es_primo = 1; //True
@@ -29,17 +31,17 @@ int main() {
 
         /* mostrar el numero y actualizar el contador */
         if (es_primo) {
-            printf("%d ", n);
             numero = n; 
+            
             primos_por_mostrar--; //Reduciendo el valor de primos
             
             /*Invertir números*/
-            int invertir=0;
+            int invertir = 0;
 
-            while(numero!=0) //721 != 0
+            while(numero != 0) //721 != 0
                 {
-                    resto = numero%10; //.1 , .2, .7
-                    numero = numero/10; //72.1, 7.2, 0 << se termina la condición del while
+                    resto = numero % 10; //.1 , .2, .7
+                    numero = numero / 10; //72.1, 7.2, 0 << se termina la condición del while
                     
                     invertir = invertir*10+resto;
                     //1ra pasada: 0 * 10 = 0 + 1 = 1
@@ -47,27 +49,33 @@ int main() {
                     //3ra pasada: 12 * 10 = 120 + 7 = 127
                 }
                 /*Comprobar si el número espejo es primo o compuesto*/
-                int count = 0;
-                for (int i = 1; i <= invertir; i++)
-                {
-                    if (invertir%i==0)
-                    {
-                        count++;
+                
+                primo_final = 1; //True
+                    for (int i = 2; i < invertir; i++) { //d contaste pa dividir; siempre y cuando d sea menor a n, aumenta el valor d
+                        if (invertir % i == 0) { //residuo igual a cero, para saber si es número exacto
+                            primo_final = 0; //False
+                            break;
+                        }
                     }
-                }
-                /*Mostrar el número espejo*/
-                if (count>2)
-                {
-                    printf("Su espejo no es primo: %d\n", invertir);
-                }
-                else
-                {
-                    printf("Su espejo es primo: %d\n", invertir);
+                
+                /* mostrar el numero y actualizar el contador */
+                if (primo_final) {
+                    printf("El número: %d ", n);
+                    
+                    numero_limite = n;
+                     
+                    printf("y su espejo: %d son primos.", invertir);
+                    printf("\nnumero limite: %d\n",numero_limite);
                 }
 
         }
-
+        
+        
+       
         n++; //Actualizar el contador, es decir si n valia 2 ahora vale 2
+        
+        printf("%d\n",n);
+        
 
     }
 
