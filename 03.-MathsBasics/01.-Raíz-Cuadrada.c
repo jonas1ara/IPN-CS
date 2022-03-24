67e4fe95ab45317b0223e5b31d63f97f6b056bbb
Jonas Lara <adrianlr2515@outlook.com>
2022-03-24 15:12:40 -0600

    RC

---
+++ 03.-MathsBasics/01.-Raíz-Cuadrada.c
@@ -0,0 +1,16 @@
+#include <math.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    double numero;
+
+    printf("Escribe un número y te dire su raíz cuadra\n");
+    scanf("%lf", &numero);
+
+    double raizCuadrada = sqrt(numero);
+    printf("La raíz cuadrada de %lf  es %lf\n", numero, raizCuadrada);
+
+    return 0;
+
+}
