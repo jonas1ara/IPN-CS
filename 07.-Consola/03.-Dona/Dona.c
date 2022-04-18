e6cbf62b704bc749c367e9110a389378be1b077c
Jonas Lara <adrianlr2515@outlook.com>
2022-04-18 16:36:50 -0500

    Dona, TTT y Hanoi Linux

--- 07.-Consola/03.-Dona/Dona.c
+++ 07.-Consola/03.-Dona/Dona.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
-#include <windows.h>
+#include <unistd.h>
 
 int main() {
     float A = 0, B = 0;
@@ -42,7 +42,7 @@ int main() {
             A += 0.00004;
             B += 0.00002;
         }
-        Sleep(0.1);
+        usleep(30000);
     }
     return 0;
 }
