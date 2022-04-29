b3ea68f866ccc72bf8dd6c31b1195365abbb9719
Jonas Lara <adrianlr2515@outlook.com>
2022-04-28 23:11:33 -0500

    bash

--- Config.sh
+++ Config.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+sudo apt update && sudo apt upgrade -y
+
 echo "Este script instala las librerías como build-essential, ncurses, gtk4, clang, tmux, curl y make. Y  configura tmux y vim para trabajar con C/C++"
