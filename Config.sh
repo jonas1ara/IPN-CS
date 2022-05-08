209e971f5bca951eae2cec16592f1033956b6726
Jonas-Lara <adrianlr2515@outlook.com>
2022-05-08 00:34:38 -0500

    Config.sh

--- Config.sh
+++ Config.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-
-
 echo "Este script instala las librerías para trabajar con este repositorio"
+
 
 sudo apt update && sudo apt upgrade -y
 
-sudo apt-get install libncurses5-dev libncursesw5-dev
+echo "Herramientas y compiladores"
 
-sudo apt install libgtk-4-1 libgtk-4-dev
+sudo apt instal build-essential gdb clang make
+
+echo "Librerías para ncurses"
+
+sudo apt-get install libncurses5-dev libncursesw5-dev -y
+
+echo "Librerías para GTK4"
+
+sudo apt install libgtk-4-1 libgtk-4-dev -y
+
