d6ea706c6ccea0c494bb698067ab7d22a216c68b
Jonas Lara <adrianlr2515@outlook.com>
2022-05-30 11:58:50 -0500

    Consola y HowTo

---
+++ 05.-HowTo/08-Impresión-Círculo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int n = 10;
+
+    for(int j = 0; j <= n*2; j++) //y
+	{
+		for(int i = 0; i<= n*2; i++) //x
+		{
+			if (pow(i-n,2) + pow(j-n,2) <= pow(n,2))
+			{
+				printf("%c",176);
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	} 
+    
+    return 0;
+}
