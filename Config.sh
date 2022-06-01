97a549ca1a133b0164e59ecf0d2a1fa9cd17d7d9
Jonas Lara <adrianlr2515@outlook.com>
2022-05-31 19:54:46 -0500

    Script de instalación de librerías

--- Config.sh
+++ Config.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 echo "Este script instala las librerías para trabajar con este repositorio"
-
+echo " "
 
 sudo apt update && sudo apt upgrade -y
 
-echo "Herramientas y compiladores"
+echo " "
+echo "Herramientas y compiladores: "
+echo " "
 
-sudo apt instal build-essential gdb clang make
+sudo apt install build-essential gdb clang make
 
-echo "Librerías para ncurses"
+echo " "
+echo "Librerías para ncurses: "
+echo " "
 
 sudo apt-get install libncurses5-dev libncursesw5-dev -y
 
-echo "Librerías para GTK4"
+echo " "
+echo "Librerías para GTK4: "
+echo " "
 
 sudo apt install libgtk-4-1 libgtk-4-dev -y
-
-
