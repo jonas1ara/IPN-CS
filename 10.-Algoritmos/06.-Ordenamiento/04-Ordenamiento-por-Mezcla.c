4bce84f4ed697c8382bb80aaebea488bc0fc3206
Jonas Lara <adrianlr2515@outlook.com>
2022-06-20 12:52:56 -0500

    Final Merge sort

--- 10.-Algoritmos/06.-Ordenamiento/04-Ordenamiento-por-Mezcla.c
+++ 10.-Algoritmos/06.-Ordenamiento/04-Ordenamiento-por-Mezcla.c
@@ -5,60 +5,81 @@
 int a[MAX] = {4,6,3,2,1,9,7};
 int b[6];
 
-void merging(int low, int mid, int high) 
-{
-   int l1, l2, i;
+void mezclando(int , int , int ); 
+void ordenar(int , int ) ;
 
-   for(l1 = low, l2 = mid + 1, i = low; l1 <= mid && l2 <= high; i++) 
+int main() 
+{ 
+
+   printf("Ordenamiento burbuja con complejidad Ο(n log n) \n\n");
+   printf("Arreglo de entrada: \n");
+   
+   for(int i = 0; i < MAX; i++)
+      printf("%d ", a[i]);
+
+   ordenar(0, MAX);
+
+   printf("\nArreglo de salida: \n");
+   
+   for(int i = 1; i <= MAX; i++)
+      printf("%d ", a[i]);
+   
+   printf("\n");
+}
+
+//Recibe por valor el inicio, la mitas y el fin del arreglo
+void mezclando(int inicio, int mitad, int final) 
+{
+   //i1 es el inicio del arreglo 1 y i2 es el inicio del arreglo 2
+   int i1, i2, i;
+
+   //Ciclo for para recorrer los vectores a y b
+   //Se inicializa i1 en el inicio del arreglo 1, se inicializa i2 en el inicio del arreglo 2
+   //Se inicializa i en el inicio del arreglo 1.
+   //Mientras que el arreglo 1 y 2 se puedan recorrer, se aumenta el valor de i 
+   for(i1 = inicio, i2 = mitad + 1, i = inicio; i1 <= mitad && i2 <= final; i++) 
    {
-      if(a[l1] <= a[l2])
-         b[i] = a[l1++];
+      //Si el valor dentro del arreglo 1 es menor que el valor dentro del arreglo 2, se copia el valor del arreglo 1 al arreglo 2
+      if(a[i1] <= a[i2])
+      //Se copia el valor del arreglo 1 al arreglo 2
+         b[i] = a[i1++];
       else
-         b[i] = a[l2++];
+      //Se copia el valor del arreglo 2 al arreglo 1
+         b[i] = a[i2++];
+
+      //Recordar que b es un arreglo vacio, que se declaro al inicio
    }
    
-   while(l1 <= mid)    
-      b[i++] = a[l1++];
+   //Ciclos while para mezclar los arreglos a y b
+   //Mientras que el arreglo 1 se pueda recorrer, se copia el valor del arreglo 1 a la siguiente posición del arreglo 2
+   while(i1 <= mitad)    
+      b[i++] = a[i1++];
 
-   while(l2 <= high)   
-      b[i++] = a[l2++];
+   //Mientras que el arreglo 2 se pueda recorrer, se copia el valor del arreglo 2 a la siguiente posición del arreglo 1
+   while(i2 <= final)   
+      b[i++] = a[i2++];
 
-   for(i = low; i <= high; i++)
+   //Se recorre el arreglo 2, copiando el valor de cada posición al arreglo 1
+   for(i = inicio; i <= final; i++)
       a[i] = b[i];
 }
 
-void sort(int low, int high) 
+//Recibe por valor dos enteros que representan el rango de elementos a ordenar
+void ordenar(int inicio, int final) 
 {
-   int mid;
-   
-   if(low < high) 
+   //Caso base; solo hay un elemento
+   if(inicio < final) 
    {
-      mid = (low + high) / 2;
-      sort(low, mid);
-      sort(mid+1, high);
-      merging(low, mid, high);
+      //Indicara la mitad del arreglo
+      int mitad = (inicio + final) / 2;
+      //Dividir la parte izquierda del arreglo
+      ordenar(inicio, mitad);
+      //Dividir la parte derecha del arreglo
+      ordenar(mitad+1, final);
+      mezclando(inicio, mitad, final);
    } else 
    { 
       return;
    }   
-}
-
-int main() 
-{ 
-   int i;
-   printf("Ordenamiento burbuja con complejidad Ο(n log n) \n\n");
-   printf("Arreglo de entrada: \n");
-   
-   for(i = 0; i < MAX; i++)
-      printf("%d ", a[i]);
-
-   sort(0, MAX);
-
-   printf("\nArreglo de salida: \n");
-   
-   for(i = 1; i <= MAX; i++)
-      printf("%d ", a[i]);
-   
-   printf("\n");
 }
 
