a368c5d62513e47b4ab557a7db8cbb918208ebc9
Jonas Lara <61485427+Jonas-Lara@users.noreply.github.com>
2022-06-30 23:23:04 -0500

    Update 02-Análisis-de-expresiones-con-Cola.c

--- 11.-Estructuras-de-Datos-en-C/2.-Colas/02-Análisis-de-expresiones-con-Cola.c
+++ 11.-Estructuras-de-Datos-en-C/2.-Colas/02-Análisis-de-expresiones-con-Cola.c
@@ -1,158 +1,96 @@
-#include<stdio.h> 
-#include<string.h> 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-//char stack
-char stack[25]; 
-int top = -1; 
+//Generar el elemento nodo
+typedef struct actividad agenda;
+struct actividad{
+     char act[50]; //Actividad a realizar...
+     char hora[10]; //Hora de la actividad...
+     agenda *siguiente; //Puntero al siguiente elemento
+};
 
-void push(char item) {
-   stack[++top] = item; 
-} 
+//Prototipos
+void Add(agenda **, agenda **, char *, char *);
+int Pull(agenda **, agenda **, char *, char *);
+int vacia(agenda *);
 
-char pop() {
-   return stack[top--]; 
-} 
-
-//returns precedence of operators
-int precedence(char symbol) {
-
-   switch(symbol) {
-      case '+': 
-      case '-':
-         return 2; 
-         break; 
-      case '*': 
-      case '/':
-         return 3; 
-         break; 
-      case '^': 
-         return 4; 
-         break; 
-      case '(': 
-      case ')': 
-      case '#':
-         return 1; 
-         break; 
-   } 
-} 
-
-//check whether the symbol is operator?
-int isOperator(char symbol) {
-
-   switch(symbol) {
-      case '+': 
-      case '-': 
-      case '*': 
-      case '/': 
-      case '^': 
-      case '(': 
-      case ')':
-         return 1; 
-      break; 
-         default:
-         return 0; 
-   } 
-} 
-
-//converts infix expression to postfix
-void convert(char infix[],char postfix[]) {
-   int i,symbol,j = 0; 
-   stack[++top] = '#'; 
-	
-   for(i = 0;i<strlen(infix);i++) {
-      symbol = infix[i]; 
-		
-      if(isOperator(symbol) == 0) {
-         postfix[j] = symbol; 
-         j++; 
-      } else {
-         if(symbol == '(') {
-            push(symbol); 
-         } else {
-            if(symbol == ')') {
-				
-               while(stack[top] != '(') {
-                  postfix[j] = pop(); 
-                  j++; 
-               } 
-					
-               pop();   //pop out (. 
-            } else {
-               if(precedence(symbol)>precedence(stack[top])) {
-                  push(symbol); 
-               } else {
-					
-                  while(precedence(symbol)<=precedence(stack[top])) {
-                     postfix[j] = pop(); 
-                     j++; 
-                  } 
-						
-                  push(symbol); 
-               }
-            }
-         }
-      }
-   }
-	
-   while(stack[top] != '#') {
-      postfix[j] = pop(); 
-      j++; 
-   } 
-	
-   postfix[j]='\0';  //null terminate string. 
-} 
-
-//int stack
-int stack_int[25]; 
-int top_int = -1; 
-
-void push_int(int item) {
-   stack_int[++top_int] = item; 
-} 
-
-char pop_int() {
-   return stack_int[top_int--]; 
-} 
-
-//evaluates postfix expression
-int evaluate(char *postfix){
-
-   char ch;
-   int i = 0,operand1,operand2;
-
-   while( (ch = postfix[i++]) != '\0') {
-	
-      if(isdigit(ch)) {
-	     push_int(ch-'0');  // Push the operand 
-      } else {
-         //Operator,pop two  operands 
-         operand2 = pop_int();
-         operand1 = pop_int();
-			
-         switch(ch) {
-            case '+':
-               push_int(operand1+operand2);
-               break;
-            case '-':
-               push_int(operand1-operand2);
-               break;
-            case '*':
-               push_int(operand1*operand2);
-               break;
-            case '/':
-               push_int(operand1/operand2);
-               break;
-         }
-      }
-   }
-	
-   return stack_int[top_int];
+//FunciÃ³n principal
+int main(void){
+  //Declaracion de variables
+  int eleccion;
+  char act[50];
+  char hora[10];
+  
+  //Crear la cola
+  agenda *inicio = NULL;
+  agenda *final = NULL;
+  
+  //Mensaje de inicio
+  printf("Este programa genera una cola\n");
+  
+  //Menu
+  while(eleccion != -1){
+  	printf("\n Menu\n1- Agregar elemento\n2- Recuperar\n-1- Salir\n\n\tElige: ");
+  	scanf("%d",&eleccion);
+  	fflush(stdin);
+  	switch(eleccion){
+  		case 1:
+  			printf("Dame la actividad a ingresar: ");
+  			gets(act);
+  			printf("Dame la hora a ingresar: ");
+  			gets(hora);
+  			Add(&inicio,&final,act,hora);
+  			break;
+  		case 2:
+  			if(Pull(&inicio,&final,act,hora))
+  				printf("La actividad y su hora recuperadas son: %s -- %s\n",act,hora);
+  			else
+  				printf("Tu agenda esta vacia\n");
+  			break;
+  	}
+  }
+  return 0;
 }
 
-void main() { 
-   char infix[25] = "1*(2+3)",postfix[25]; 
-   convert(infix,postfix); 
+//Función que agrega un elemento a la cola
+void Add(agenda **inicio, agenda **final, char *cadena, char *hora){
+	//printf("Agregando %s...\n",cadena);
+	//Reservar memoria
+	agenda *nuevoNodo = (agenda *)malloc(sizeof(agenda));
+	//Agregar el dato al nodo
+	strcpy(nuevoNodo->act,cadena);
+	strcpy(nuevoNodo->hora,hora);
+	//Ahora el nuevoNodo es el final de la cola y el siguiente es nulo
+	nuevoNodo->siguiente = NULL;
 	
-   printf("Infix expression is: %s\n" , infix);
-   printf("Postfix expression is: %s\n" , postfix);
-   printf("Evaluated expression is: %d\n" , evaluate(postfix));
+	if(vacia(*final)){ //La cola está vacía?
+		(*final) = nuevoNodo;
+		(*inicio) = nuevoNodo;
+	} else{
+		(*final)->siguiente = nuevoNodo;
+		(*final) = nuevoNodo;
+	}
+}
+
+int Pull(agenda **inicio, agenda **final, char *cadena, char *hora){
+	if(!(vacia(*inicio))){//Si la pila NO esta vacía
+		strcpy(cadena,(*inicio)->act);
+		strcpy(hora,(*inicio)->hora);
+		//Hacemos referencia al nodo de inicio para poder liberar la memoria
+		agenda *temporal = *inicio;
+		//Inicio apunta a siguiente
+		(*inicio) = temporal->siguiente;
+		if((*inicio)==NULL) //Solo había un nodo?
+			(*final)=NULL;
+		//liberar memoria
+		free(temporal);
+		//printf("Retirando %s...\n",cadena);
+		return 1;
+	}
+	return 0;
+}
+
+int vacia(agenda *cola){
+	return cola==NULL;
+}
