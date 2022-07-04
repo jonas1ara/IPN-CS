083de604e2cfbc64d827cb98ea82768705e2744a
Jonas Lara <61485427+Jonas-Lara@users.noreply.github.com>
2022-07-04 16:05:19 -0500

    Update 02-Pila-dinamica.c

--- 11.-Estructuras-de-Datos-en-C/02.-Pilas/02-Pila-dinamica.c
+++ 11.-Estructuras-de-Datos-en-C/02.-Pilas/02-Pila-dinamica.c
@@ -1,8 +1,17 @@
-#include <stdio.h>    
-#include <stdlib.h>   
-#include <stdbool.h>  
+/*
+Estructuras de datos en C: implementación de pila de enteros
+Recuerda que el último en entrar es el primero en salir
+LIFO: https://es.wikipedia.org/wiki/Last_in,_first_out
+Basado en las operaciones que dice la Wikipedia:
+https://es.wikipedia.org/wiki/Pila_(inform%C3%A1tica)#Operaciones
+@author parzibyte
+Visita: parzibyte.me
+*/
+#include <stdio.h>   // printf
+#include <stdlib.h>  // malloc y free
+#include <stdbool.h> // Booleanos
 
-// Estructura del nodo de una pila
+// Un nodo tiene un dato, el cual es el número. Y otro nodo al que apunta
 struct nodo {
   int numero;
   struct nodo *siguiente;
@@ -11,146 +20,104 @@ struct nodo {
 // Prototipos de funciones
 void agregar(int numero);  // push
 void eliminarUltimo(void); // pop
-void imprimirPila(void);
-void imprimirArreglo(int arr[], int n) ;
-void ordenamiento_por_Inserccion(int arr[], int n);
-int ultimo(void);  // Función entera que devuelve el último elemento de la pila, de utilidad para el algoritmo de ordenamiento por selección
-
+void imprimir(void);
+int tamanio(void); // El tamaño de la pila
+bool vacia(void);  // Indica si la pila está vacía
+int ultimo(void);  // El último elemento. Devuelve 0 si no hay elementos
 
 // Todo comienza con el nodo superior
 struct nodo *superior = NULL;
 
 int main() {
-
-    int arr[]= {}; // Arreglo que contiene los elementos extraídos de la pila 
-    int eleccion;
-    int numero;
-    int n;
-    printf("Bienvenido al programa de ordenamiento por insercción mediante una pila dinámica\n\n");
-    printf("Por favor ingresa el número de elementos que tendrá la pila: ");
-    scanf("%d", &n);
-    printf("\n");
-    for (int i = 0; i < n; i++)
-    {
-        printf("Ingresa el número que agregaremos a la pila:\n");
-        scanf("%d", &numero);
-        agregar(numero);
+  int eleccion;
+  int numero;
+  while (eleccion != -1) {
+    printf("\n--BIENVENIDO--\n1.- Agregar\n2.- Eliminar\n3.- Imprimir "
+           "pila\n4.- Imprimir tamaño\n5.- Comprobar si está vacía\n6.- "
+           "Mostrar último elemento\n-1.- Salir\n\n\tElige: ");
+    scanf("%d", &eleccion);
+    switch (eleccion) {
+    case 1:
+      printf("Ingresa el número que agregaremos:\n");
+      scanf("%d", &numero);
+      agregar(numero);
+      break;
+    case 2:
+      eliminarUltimo();
+      break;
+    case 3:
+      imprimir();
+      break;
+    case 4:
+      printf("La pila mide %d\n", tamanio());
+      break;
+    case 5:
+      if (vacia()) {
+        printf("La pila está vacía\n");
+      } else {
+        printf("La pila NO está vacía\n");
+      }
+      break;
+    case 6:
+      printf("El último elemento es: %d\n", ultimo());
+      break;
     }
-    imprimirPila();
-    for (int i = 0; i < n; i++)
-    {
-       printf("Eliminando el último elemento: %d , ", ultimo());
-
-        arr[i] = ultimo();
-
-        eliminarUltimo();
-        imprimirPila();
-    }
-    printf("Nada por imprimir, el arreglo obtenido para ordenar por el método de insercción es el siguiente:\n");
-    imprimirArreglo(arr , n);
-    ordenamiento_por_Inserccion(arr, n);
-    
-    printf("Arreglo ordenado por el método de insercción:\n");
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d ", arr[i]);
-
-        agregar(arr[i]); //A la vez que se va recorriendo el arreglo ordenado, se agregan los elementos en orden a la pila
-    }
-    printf("\n\n"); //Brinquito para que no se vea tan malo el resultado jaja :v
-    
-    imprimirPila();
-
-    return 0;
-    
+  }
 }
 
-//Función para saber cuál es el último elemento(número) de la pila
-int ultimo() 
-{
-    if (superior == NULL)
+int tamanio(void) {
+  int contador = 0;
+  if (superior == NULL)
+    return contador;
+  struct nodo *temporal = superior;
+  while (temporal != NULL) {
+    contador++;
+    temporal = temporal->siguiente;
+  }
+  return contador;
+}
+
+bool vacia(void) { return superior == NULL; }
+
+int ultimo() {
+  if (superior == NULL)
     return -1;
-    return superior->numero;
+  return superior->numero;
 }
 
-//Operación push
-void agregar(int numero) 
-{
-  // Reservamos memoria para el nuevo nodo, que se agregará a la pila
+// Operación push
+void agregar(int numero) {
+  printf("Agregando %d\n", numero);
+  // El que se agregará; reservamos memoria
   struct nodo *nuevoNodo = malloc(sizeof(struct nodo));
-  // Asignamos el valor del nuevo nodo
+  // Le ponemos el dato
   nuevoNodo->numero = numero;
-  // Y ahora el nuevo nodo es el superior, y su siguiente es el que antes era el superior
+  // Y ahora el nuevo nodo es el superior, y su siguiente
+  // es el que antes era superior
   nuevoNodo->siguiente = superior;
   superior = nuevoNodo;
 }
 
-// Operación pop, eliminar el de hasta arriba
-void eliminarUltimo(void) 
-{
-  printf("elemento eliminado.\n"); //Para confirmar que el elemento fue eliminado
-  if (superior != NULL) {
-    // Para liberar la memoria más tarde debemos referencias al espacio de memoria del nodo a eliminar y asi no equivocadamente eliminar el que sigue
-    struct nodo *temporal = superior;
-    // Ahora superior es a lo que apuntaba su siguiente
-    superior = superior->siguiente;
-
-    // Liberar memoria que estaba ocupando el que eliminamos (la referencia de memoria)
-    free(temporal);
-  }
-}
-
-//Función para imprimir la pila
-void imprimirPila(void) 
-{
-  printf("Imprimiendo pila ...\n\n");
-  struct nodo *temporal = superior; // Guardamos el nodo superior en una variable temporal
-  //Mientras el nodo temporal no sea nulo, imprimimos su valor y movemos el puntero al siguiente nodo
+void imprimir(void) {
+  printf("Imprimiendo...\n");
+  struct nodo *temporal = superior;
   while (temporal != NULL) {
     printf("%d\n", temporal->numero);
     temporal = temporal->siguiente;
   }
-  printf("\n");
 }
 
-//Función para imprimir arreglo utilizado para el ordenamiento, le pasamos el arreglo y el número de elementos que contiene
-void imprimirArreglo(int arr[], int n) 
-{
-   // Navegar a tráves de todos los elementos del arreglo
-   for(int i = 0;i < n ;i++) 
-   {
-      printf("%d ", arr[i]);
-   }
-	
-   printf("\n\n");
-}
+// Operación pop, eliminar el de hasta arriba
+void eliminarUltimo(void) {
+  printf("Eliminando el último\n");
+  if (superior != NULL) {
+    // Para liberar la memoria más tarde debemos
+    // tener la referencia al que vamos a eliminar
+    struct nodo *temporal = superior;
+    // Ahora superior es a lo que apuntaba su siguiente
+    superior = superior->siguiente;
 
-//Función para ordenar por selección, le pasamos el arreglo y el número de elementos que contiene
-void ordenamiento_por_Inserccion(int arr[], int n) 
-{
-   int valor_a_insertar;
-   int posicion_a_insertar;
-  
-   // Ciclo a tráves de todos los números del arreglo
-   for(int i = 1; i < n; i++) 
-   { 
-      // Selecciona un valor para ser insertado 
-      valor_a_insertar = arr[i];
-		
-      // Selecciona el espacio donde el valor será insertado 
-      posicion_a_insertar = i;
-		
-      // Comprobar si el anterior no es mayor que el valor a insertar 
-      while (posicion_a_insertar > 0 && arr[posicion_a_insertar-1] > valor_a_insertar) 
-      {
-         arr[posicion_a_insertar] = arr[posicion_a_insertar-1];
-         posicion_a_insertar--;
-      }
-
-      if(posicion_a_insertar != i) 
-      {
-         // Inserte el número en la posición del agujero
-         arr[posicion_a_insertar] = valor_a_insertar;
-      }
-   }  
+    // Liberar memoria que estaba ocupando el que eliminamos
+    free(temporal);
+  }
 }
