9503b009b80104665f29e5859eb2c806c23b79d2
Jonas Lara <61485427+Jonas-Lara@users.noreply.github.com>
2022-11-13 15:27:10 -0600

    Update 01-For.c

--- 01.-Básicos-del-lenguaje-C/12.-Bucles/01-For.c
+++ 01.-Básicos-del-lenguaje-C/12.-Bucles/01-For.c
@@ -1,19 +1,21 @@
 /*
-	Ciclo for: El bucle for es una estructura de control en programación
+	Ciclo for 
+	
+	El bucle for es una estructura de control en programación
 	en la que se puede indicar de antemano el 
 	número máximo de iteraciones
 */
 
 #include <stdio.h>
 
-int main(){
-	
+int main()
+{	
 	int contador; //Se inicializa la variable de tipo entero contador
+	
 	for(contador = 1; contador <= 10; contador++) //Se le asigna 1 a contador, se condiciona que contador sea menor o igual a 10 y si en cada repetición contador va a adherirse 1
 	{
 		printf("%i\n",contador);
 	}
 
 	return 0;
-
 }
