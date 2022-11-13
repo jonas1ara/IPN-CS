16bc63fa477e3da5b0014b64a8480985051c1601
Jonas Lara <61485427+Jonas-Lara@users.noreply.github.com>
2022-11-13 15:42:43 -0600

    Update 02-FunciónRand.c

--- 01.-Básicos-del-lenguaje-C/13.-Funciones/02-FunciónRand.c
+++ 01.-Básicos-del-lenguaje-C/13.-Funciones/02-FunciónRand.c
@@ -1,15 +1,19 @@
+/*
+   Función rand para generar números pseudoaleatorios
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <time.h> //Directiva necesaria para llamar a la función RAND
 
 int main () 
 {
    int i, n;
-   time_t t;
+   time_t t; //Palabra reserva time_t de la libería time.h
    
    n = 5;
    
-   /* Inicia el generador de número aleatorio con la semilla del tiempo actual */
+   // Inicia el generador de número aleatorio con la semilla del tiempo actual 
    srand((unsigned) time(&t));
 
    /* Imprime 5 números aleatorios del 0 al 49 */
