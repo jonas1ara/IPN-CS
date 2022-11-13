26e23a38eb8ceb6c9ff3f58716bfdafd7e0f6627
Jonas Lara <61485427+Jonas-Lara@users.noreply.github.com>
2022-11-13 15:22:34 -0600

    Update 06-Convertir.c

--- 01.-Básicos-del-lenguaje-C/06-Convertir.c
+++ 01.-Básicos-del-lenguaje-C/06-Convertir.c
@@ -4,8 +4,8 @@
 
 #include <stdio.h>
 
-int main () {
-	
+int main () 
+{	
 	int a = 80; //Se inicializa la variable a en 80
 	float f = 45.3; //Se inicializa la variable flotante 43.5
 	float sum = (float)a + f; //Aderimos (float)a para convertir el entero a en flotante y sumarlo con el entero f
@@ -15,6 +15,4 @@ int main () {
 	printf("\nY estos son el entero convertido: \nEntero: %i\nFlotante: %.2f\nDoble: %.3f\nCarácter: %c\n",a, (float)a, (double)a, (char)a);
 
 	return 0;
-
-
 }
