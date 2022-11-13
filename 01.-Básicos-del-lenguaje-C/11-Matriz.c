9de358e3ba4705fd047c0e0b27441c259ad87aed
Jonas Lara <61485427+Jonas-Lara@users.noreply.github.com>
2022-11-13 15:26:27 -0600

    Update 11-Matriz.c

--- 01.-Básicos-del-lenguaje-C/11-Matriz.c
+++ 01.-Básicos-del-lenguaje-C/11-Matriz.c
@@ -1,11 +1,12 @@
 /*
+	Matriz, arreglos en dos dimensiones
+	
 	Los arreglos son un conjunto de datos que se van almacenando dentro de un variable,
-	cuándo hablamos de variables imaginalo como una cajita donde se almacenan cosas, 
+	cuándo hablamos de arreglos imaginalo como una cajita donde se almacenan cosas, 
 	se pueden modificar sus propiedades pero siempre de un solo tipo, los ARREGLOS
 	nos permiten guardar muchos de estos datos, en ese caso son conocidos como 
 	una entidad estática, ya que al declararlas no van a cambiar su tamaño
-	mientras dura la ejecución del programa y recuerda que 
-	siempre inician en la posición 0.
+	mientras dura la ejecución del programa y recuerda que siempre inician en la posición 0.
 	Arreglo multidimensional, piensa en los arreglos multidimensionales como coordenadas y 
 	recuerda que este se define por tener dos corchetes.
 */
@@ -16,9 +17,9 @@ int main()
 {
 	/*	
 		Sistemas de coordenadas
-			Col... 0 1 2 // Columnas tienen el valor vertical
-			Fila0 |5 3 1 // Valor horizontal
-			Fila1 |6 4 2 // Valor horizantal
+			Col...  0 1 2 // Columnas tienen el valor vertical
+			Fila 0 |5 3 1 // Valor horizontal
+			Fila 1 |6 4 2 // Valor horizantal
 	*/
 	int multi[2][3] = {{5, 3, 1},{6, 4, 2}}; 
 	//La primeras llaves  quedan en la fila 0 y así sucesivamente, dentro de una llave que engloba las dos filas
