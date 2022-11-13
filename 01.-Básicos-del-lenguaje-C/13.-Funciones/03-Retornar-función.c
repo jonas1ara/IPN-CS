9a3c1c47b2ff5a41f0622641a3ea5496f1cf1827
Jonas Lara <61485427+Jonas-Lara@users.noreply.github.com>
2022-11-13 15:32:27 -0600

    Update 03-Retornar-función.c

--- 01.-Básicos-del-lenguaje-C/13.-Funciones/03-Retornar-función.c
+++ 01.-Básicos-del-lenguaje-C/13.-Funciones/03-Retornar-función.c
@@ -1,6 +1,10 @@
-#include <stdio.h> //directiva include
+/*
+	Retornar el valor de una función
+*/
 
-int sum(); //función suma
+#include <stdio.h> 
+
+int sum(); //prototipo de la función suma
 int num3 = 5; //variable global de tipo entero
 
 int main() 
@@ -13,16 +17,14 @@ int main()
 	printf("Ingresa el segundo valor:\n");
 	scanf("%i", &num2);
 	
-	printf("El resultado es: %i\n", sum(num1,num2)); // Imprimir lo que retorna la función suma	
+	printf("El resultado es: %i\n", sum(num1,num2)); // Imprimir lo que retorna la función suma pasandole el valor de num1 y num2	
 
 	return 0;
-
 }
 
 int sum (int num1, int num2) 
 {	
-	int sum = num1 + num2 + num3; //Sumar variable globar
+	int suma = num1 + num2 + num3; //Sumar variable globar
 
-	return sum;
-
+	return suma; //Retornar el valor de sum
 }
