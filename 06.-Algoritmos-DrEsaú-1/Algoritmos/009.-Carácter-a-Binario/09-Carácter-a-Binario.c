f21b284e86b2a2288d005ba74a7f1b0d91fbe872
Jonas Lara <adrianlr2515@outlook.com>
2023-02-26 17:31:09 -0600

    Update

--- 06.-Algoritmos-DrEsaú-1/Algoritmos/009.-Carácter-a-Binario/09-Carácter-a-Binario.c
+++ 06.-Algoritmos-DrEsaú-1/Algoritmos/009.-Carácter-a-Binario/09-Carácter-a-Binario.c
@@ -1,34 +1,29 @@
-/*
-	Nombre del programa: charbin.c 
-	Fecha: 6/13/21
-	Autor: Adrián Jonás Lara Carvente
-	Descripción breve: Este programa convierte un carácter a binario.
-*/
+#include <stdio.h>  
+#include <string.h>
 
-#include <stdio.h>  //Biblioteca de entradas y salidas estandar.
-#include <string.h>//biblioteca para uso de cadenas de texto en C 
+int main () 
+{
 
-int main () {
+	char texto[20];//Declaro la variable de tipo char texto con un máximo de 20 carácteres. 
+	int i, j; //Declaro variables i y j como enteros.
+	printf("Este programa convierte cualquier carácter a binario\n");
+	printf("Introduzca su carácter u oracion de 20 carácteres máximo: "); //Le pido al usuario 
+	scanf("%[^\n]",&texto); //Ocupamos %[^\n] para poder hacer la lectura de caracteres y hacemos uso de salto de página. 
+	fflush(stdin); //Limpiamos la memoria 
+	char *str = texto; //El valor de texto se lo asigno a *str
+	int largo = strlen(str);//Ocupo la funcion strlen ya que devuelve el número. 
 
-char texto[20];//Declaro la variable de tipo char texto con un máximo de 20 carácteres. 
-int i, j; //Declaro variables i y j como enteros.
-printf("Este programa converite cualquier carácter a binario\n"); //Ilustramos al usuario
-printf("Introduzca su oracion: "); //Le pido al usuario 
-scanf("%[^\n]",&texto);//Ocupamos %[^\n] para poder hacer la lectura de caracteres y hacemos uso de salto de página. 
-fflush(stdin); //Limpiamos la memoria 
-char *str = texto; //El valor de texto se lo asigno a *str
-int largo = strlen(str);//Ocupo la funcion strlen ya que devuelve el número. 
-
-for (i = 0; i < largo; i++) //Iniciamos un ciclo for para convertir a codigo ASCII
- {
-   for(j=7;j>=0;j--)//Impresión binaria de 0 a 7 porque esos son los números de bits.
-    {
-    if((str[i]>>j&1)==1) //Condición para imprimir 1 o 0.
-      printf("1"); 
-    else 
-      printf("0"); 
-    }
-  printf(" = %c  \n", str[i] ); //Imprimimos nuestra cadena de caracter
-}
-return 0;
+	for (i = 0; i < largo; i++) //Iniciamos un ciclo for para recorrer
+	 {
+	   for(j=7;j>=0;j--)//Impresión binaria de 0 a 7 porque esos son los números de bits.
+		{
+			if((str[i]>>j&1)==1) //Condición para imprimir 1 o 0.
+			  printf("1"); 
+			else 
+			  printf("0"); 
+		}
+	  printf(" = %c  \n", str[i] ); //Imprimimos nuestra cadena de caracter
+	}
+	
+	return 0;
 }
--- 06.-Algoritmos-DrEsaú-1/Algoritmos/009.-Carácter-a-Binario/README.md
+++ 06.-Algoritmos-DrEsaú-1/Algoritmos/009.-Carácter-a-Binario/README.md
@@ -1,12 +1,30 @@
-# Ipn.c
+# Carácer a Binario
 
-![](/00.-Sources/Ipn.svg)
+![](/00.-Sources/Images/binario.PNG)
 
-_asdfghjklñ_
 
-## Requisitos 📋
+_Para convertir un carácter a su representación binaria utilizando ciclos for, se puede utilizar la misma técnica que se utiliza con el operador ">>" y "&", pero se implementa manualmente. Es decir, se recorre cada bit del número entero correspondiente al carácter y se compara su valor con una potencia de 2._
 
-_Herramientas que necesita para usar este repositorio y cómo instalarlo_
+_La siguiente función muestra cómo se puede hacer esto utilizando ciclos for:_
 
-![alt text](/00.-Sources/vs.png)
+```
+void car_a_binario(char c) {
+    int i, j;
+    for (i = 7; i >= 0; i--) {
+        j = 1 << i;
+        if (c & j) {
+            printf("1");
+        } else {
+            printf("0");
+        }
+    }
+    printf("\n");
+}
+
+```
+
+_En esta función, se utiliza un bucle for para recorrer los bits del número entero correspondiente al carácter de izquierda a derecha. Dentro del bucle, se utiliza la variable j para almacenar el valor de la potencia de 2 correspondiente al bit actual. Luego, se utiliza el operador "&" para comparar el bit actual con el valor j. Si el resultado es distinto de cero, significa que el bit está activado y se imprime un 1. De lo contrario, se imprime un 0._
+
+
+
 
