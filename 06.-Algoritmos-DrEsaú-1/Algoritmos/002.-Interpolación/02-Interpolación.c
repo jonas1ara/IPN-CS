4895acedb6b849518c2dfeab3899a71609c1cdd4
Jonas Lara <adrianlr2515@outlook.com>
2023-02-26 15:03:57 -0600

    Update

--- 06.-Algoritmos-DrEsaú-1/Algoritmos/002.-Interpolación/02-Interpolación.c
+++ 06.-Algoritmos-DrEsaú-1/Algoritmos/002.-Interpolación/02-Interpolación.c
@@ -1,35 +1,26 @@
-/*
-
-	Name: Interpolation
-	Date: 25/04/21 20:00
-	Author: Adrián Jonás Lara Carvente 
-	Description: This program calculates quadratic interpolation
-	based on three points on a plane
-*/
-
 #include <stdio.h>
 
 int main ()
 {
-//I declare float variables 
-float x, y, x0= 2014, y0=3000, x1=2018, y1=3800, x2= 2019,
-y2=4100;
-//I explain to the user what to do
-printf("Este programa te da la matricula de un año entre el 2000 y 2030\n"); 
-//The user enters the year and saves in variable X
-printf("Escribe el año del cual deseas saber la matricula: \n" );
-scanf("%f",&x);
-//I condition the program so that it has a minimum and a maximum of years
-if (x>=2000 && x<=2030) {
-//Interpolation in variable Y, I just transcribed equation
-y=(y0*(((x-x1)*(x-x2))/((x0-x1)*(x0-x2))))+(y1*(((x-x0)*(x-x2))/((x1-x0)*(x1-x2))))+(y2*(((x-x0)*(x-x1))/((x2-x0)*(x2-
-x1))));
-//Prints result
-printf("El numero de alumnos matriculados es: %.2f\n",y);
-} 
-//Help the user
-else 
-printf("Este programa solo cálcula la matricula entre el año 2000 al año 2030,por favor ingresa un año valido.\n");
+ 
+	float x, y, x0= 2014, y0=3000, x1=2018, y1=3800, x2= 2019, y2=4100;
+
+	printf("Este programa te da la matricula de un año entre el 2000 y 2030\n"); 
+	printf("Escribe el año del cual deseas saber la matricula: \n" );
+	scanf("%f",&x);
+	
+	if (x>=2000 && x<=2030) 
+	{
+	
+	//Interpolación cuadrática
+	y=(y0*(((x-x1)*(x-x2))/((x0-x1)*(x0-x2))))+(y1*(((x-x0)*(x-x2))/((x1-x0)*(x1 x2))))+(y2*(((x-x0)*(x-x1))/((x2-x0)*(x2-x1))));
+
+	printf("El numero de alumnos matriculados es: %.2f\n",y);
+	
+	} 
+	else 
+
+	printf("Este programa solo cálcula la matricula entre el año 2000 al año 2030,por favor ingresa un año valido.\n");
  
  return 0;
 }
