9a477428092c3b4ea068410e50665865e6d2ef4a
Jonas Lara <adrianlr2515@outlook.com>
2023-03-02 12:03:01 -0600

    3

--- 01.-Básicos-del-lenguaje-C/07.-Archivos/03-Escribir-archivo-con-fprintf.c
+++ 01.-Básicos-del-lenguaje-C/07.-Archivos/03-Escribir-archivo-con-fprintf.c
@@ -3,9 +3,40 @@
 #include <string.h>
 #include <unistd.h>
     
+void cambio(char aux[]);
+
 int main(int argc, char **argv)
 {
+    char aux[100];
+
+    FILE *archivo;
+
+    archivo = fopen("Ejemplo3.txt", "w");
+    if (archivo == NULL)
+    {
+        printf("Error al abrir el archivo\n");
+        exit(1);
+    }
+
+    printf("Escribe algo: ");
+    fgets(aux, 100, stdin);
     
+    fprintf(archivo, "%s", aux);
+
+    fclose(archivo);
     
     return 0;
+}
+
+void cambio(char aux[])
+{
+    int i;
+
+    for (i = 0; i < 100; i++)
+    {
+        if (aux[i] == '\n')
+        {
+            aux[i] = '\0';
+        }
+    }
 }
