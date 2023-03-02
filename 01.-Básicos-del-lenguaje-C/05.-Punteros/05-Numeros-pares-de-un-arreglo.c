23609365eb92e08bdada19a4910f0ec603daa6bb
Jonas Lara <61485427+Jonas-Lara@users.noreply.github.com>
2023-03-02 15:19:06 -0600

    Update 05-Numeros-pares-de-un-arreglo.c

--- 01.-Básicos-del-lenguaje-C/05.-Punteros/05-Numeros-pares-de-un-arreglo.c
+++ 01.-Básicos-del-lenguaje-C/05.-Punteros/05-Numeros-pares-de-un-arreglo.c
@@ -6,7 +6,24 @@
 int main(int argc, char **argv)
 {
     
-    
+    int elements;
+    printf("Cantidad de números: ");
+    scanf("%d",&elements);
+
+    int numeros[elements];
+    for(int i=0; i<elements; i++) {
+        printf("Número: %d", i+1);
+        numeros[i];
+    }
+
+    int *dir_numeros = numeros;
+    int menor = numeros[0];
+    for(int i=0; i<elements; i++) {
+        if(*dir_numeros < menor) menor = *dir_numeros;
+        dir_numeros++;
+    }
+
+    printf("Número menor: %d\n", menor);
     
     return 0;
 }
